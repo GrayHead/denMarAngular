251fa999f64597f642084f3ce64f31c7a3dee527
GrayHead <ss.zhuravlov@gmail.com>
2021-02-03 15:53:53 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'denMarAngular';
+export class AppComponent implements OnInit {
+  title = 'okten';
+  users;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(value => this.users = value);
+  }
 }
