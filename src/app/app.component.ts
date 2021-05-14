bf1fbffa743e9543e7d2f5a9e2f07774cab77333
GrayHead <ss.zhuravlov@gmail.com>
2021-05-14 15:44:25 +0300

    join w/java

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'denMarAngular';
+export class AppComponent implements OnInit {
+
+  users: any[];
+
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get<any[]>('http://localhost:8080/users').subscribe(value => this.users = value);
+  }
+
+  save(): void {
+    this.httpClient.post<any>('http://localhost:8080/users', {name: 'kokos', post: {title: 'title', body: 'body'}}).subscribe(value => {
+      console.log(value);
+    });
+
+  }
 }
